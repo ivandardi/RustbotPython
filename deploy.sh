5fbf18bdc6ff94b215df53c0f75bbd13527d63a3
Ivan Borgia Dardi <melo@48d238ca3594>
2018-05-23 20:05:00 +0000

    Fix service files

--- deploy.sh
+++ deploy.sh
@@ -4,19 +4,20 @@ set -eu
 repo_dir="$(dirname "$0")"
 dest_dir=/home/ferris/prod
 
-rm -r "$dest_dir"
+sudo rm -rf "$dest_dir"
 mkdir -p "$dest_dir"
 cp -a "$repo_dir" "$dest_dir"
-chown -R ferris:ferris "$dest_dir"
+sudo chown -R ferris:ferris "$dest_dir"
 echo "Installed source code to '$dest_dir'"
 
+cd "$dest_dir"
 pipenv install > /dev/null
 echo "Installed Python dependencies"
 
-install -m644 "$repo_dir/rustbot.service" /usr/local/lib/systemd/system/rustbot.service
-chown root:root /usr/local/lib/systemd/system/rustbot.service
+sudo install -m644 rustbot.service /usr/local/lib/systemd/system/rustbot.service
+sudo chown root:root /usr/local/lib/systemd/system/rustbot.service
 echo "Installed systemd service"
 
-systemctl daemon-reload
-systemctl restart rustbot.service
+sudo systemctl daemon-reload
+sudo systemctl restart rustbot.service
 echo "Started rustbot systemd service"
