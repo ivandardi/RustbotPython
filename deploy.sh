5e4584c94ec81ea916f20fec7ab8455f6f6d48b6
Ivan Dardi <ivy.dardi@gmail.com>
2018-07-15 03:13:28 -0300

    Try to fix deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -10,13 +10,13 @@ dest_dir=/home/ferris/prod
 sudo rm -rf "$dest_dir"
 mkdir -p "$dest_dir"
 cp -a "$repo_dir" "$dest_dir"
-sudo chown -R ferris:ferris "$dest_dir"
 echo "Installed source code to '$dest_dir'"
 
 cd "$dest_dir"
 pipenv install --pre > /dev/null
 echo "Installed Python dependencies"
 
+sudo chown -R ferris:ferris "$dest_dir"
 sudo install -m644 rustbot.service /usr/local/lib/systemd/system/rustbot.service
 sudo chown root:root /usr/local/lib/systemd/system/rustbot.service
 echo "Installed systemd service"
