9bc2a7615bf3c9d0c555deedbf12fbf29e370f7e
Ivan Dardi <ivy.dardi@gmail.com>
2018-08-18 19:59:03 -0300

    Remove timer command

--- bot/cogs/meta.py
+++ bot/cogs/meta.py
@@ -1,64 +1,7 @@
-import asyncio
 import datetime
-import re
-import random
 
 import discord
 from discord.ext import commands
-
-
-class TimeParser:
-    negative_times = [
-        "I don't do negative time",
-        "Negative time? Time travel? Pfft Marty, that's just silly!",
-        "Quick! To the Delorean! ... Oh wait, we don't have one 🤔",
-        "NEGATIVE TIME IS UNSAFE 😭",
-        "Always positive time pls",
-        "NEGATIVE TIME? BAAAAAAAAAAAAAAANNE!",
-    ]
-
-
-too_big_times = [
-    "That's a bit too far in the future for me",
-    "If you need to be remembered that far in the future, maaaybe use a calendar?",
-    "The limit is 7 days :v",
-    "REMINDER IN MORE THAN 7 DAYS? BAAAAAAAAAAAAAANNE!",
-]
-
-
-def __init__(self, argument):
-    compiled = re.compile(
-        r"(?:(?P<days>\d+)d)?(?:(?P<hours>\d+)h)?(?:(?P<minutes>\d+)m)?(?:(?P<seconds>\d+)s)?"
-    )
-    self.original = argument
-    try:
-        self.seconds = int(argument)
-    except ValueError as e:
-        match = compiled.match(argument)
-        if match is None or not match.group(0):
-            raise commands.BadArgument("Failed to parse time.") from e
-
-        self.seconds = 0
-        days = match.group("days")
-        if days is not None:
-            self.seconds += int(days) * 86400
-        hours = match.group("hours")
-        if hours is not None:
-            self.seconds += int(hours) * 3600
-        minutes = match.group("minutes")
-        if minutes is not None:
-            self.seconds += int(minutes) * 60
-        seconds = match.group("seconds")
-        if seconds is not None:
-            self.seconds += int(seconds)
-
-    if self.seconds < 0:
-        error_msg = random.choice(self.negative_times)
-        raise commands.BadArgument(error_msg)
-
-    if self.seconds > 7 * 24 * 60 * 60:  # 7 days
-        error_msg = random.choice(self.too_big_times)
-        raise commands.BadArgument(error_msg)
 
 
 class Meta:
@@ -85,31 +28,6 @@ class Meta:
                 fmt.format(d=days, h=hours, m=minutes, s=seconds)
             )
         )
-
-    @commands.command(aliases=["reminder", "remind"])
-    async def timer(
-            self, ctx: commands.Context, time: TimeParser, *, message="something"
-    ):
-        """Reminds you of something after a certain amount of time.
-        The time can optionally be specified with units such as 'h'
-        for hours, 'm' for minutes and 's' for seconds. If no unit
-        is given then it is assumed to be seconds. You can also combine
-        multiple units together, e.g. 2h4m10s.
-        The time can optionally be specified with units such as 'd' for days,
-        'h' for hours, 'm' for minutes and 's' for seconds.
-        If no unit is given then it is assumed to be seconds. You can also
-        combine multiple units together, e.g. 3d2h4m10s.
-        The maximum time limit is 7 days.
-        """
-
-        author = ctx.message.author
-        message = message.replace("@everyone", "@\u200beveryone").replace(
-            "@here", "@\u200bhere"
-        )
-
-        await ctx.send(f"""Okay {author.mention}, I'll remind you about "{message}" in {time.seconds} seconds.""")
-        await asyncio.sleep(time.seconds)
-        await ctx.send(f'Time is up {author.mention}! You asked to be reminded about "{message}".')
 
     @commands.command(aliases=["wustify"])
     @commands.guild_only()
