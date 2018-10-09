4288e87c490aceb4c3bc0a879e5ca03b166e68f1
Wesley Norris <repnop@outlook.com>
2018-10-08 20:59:20 -0400

    Adds eval command and tidys up the stderr output.

--- bot/cogs/playground.py
+++ bot/cogs/playground.py
@@ -22,6 +22,19 @@ class CodeBlock:
 
         self.source = code.rstrip("`")
 
+class CodeSection:
+    missing_error = "Missing code section. Please use the following markdown\n\\`code here\\`"
+
+    def __init__(self, code):
+        codeblock = code.startswith("```") and code.endswith("```")
+        codesection = code.startswith("`") and code.endswith("`")
+        if not codesection and not codeblock:
+            raise commands.BadArgument(self.missing_error)
+
+        if codeblock:
+            self.source = code.split('\n')[1].rstrip("`")
+        else:
+            self.source = code.strip("`")
 
 class Playground:
     """Evaluates Rust code.
@@ -68,7 +81,8 @@ class Playground:
                 if "error" in response:
                     raise commands.CommandError(response["error"])
 
-                full_response = response["stderr"] + response["stdout"]
+                full_response = response["stdout"] if len(response["stderr"].split('\n')) <= 4 else "\n".join(response["stderr"].split('\n')[1:-7])
+                # response["stderr"] + response["stdout"]
                 len_response = len(full_response)
                 if len_response < 1990:
                     msg = f"```rs\n{full_response}```"
@@ -100,6 +114,82 @@ class Playground:
         if isinstance(error, commands.CommandError):
             await ctx.send(str(error))
 
+class SingleLinePlayground:
+    """Evaluates Rust code.
+
+    Usage:
+
+    ?eval ```rs
+    <Rust code here>```
+
+    """
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        self.session = aiohttp.ClientSession(loop=self.bot.loop)
+
+    def __unload(self):
+        self.session.close()
+
+    @commands.command()
+    async def eval(self, ctx: commands.Context, *, code: CodeSection):
+        """Evaluates a single line of Rust code. Exactly equal to https://play.integer32.com/"""
+
+        async with ctx.typing():
+            payload = json.dumps(
+                {
+                    "channel": "nightly",
+                    "code": 'fn main(){println!("{:?}",{' + code.source + '});}',
+                    "crateType": "bin",
+                    "mode": "debug",
+                    "tests": False,
+                }
+            )
+
+            async with self.session.post(
+                    "https://play.integer32.com/execute", data=payload
+            ) as r:
+                if r.status != 200:
+                    raise commands.CommandError(
+                        "Rust i32 Playground didn't respond in time."
+                    )
+
+                response = await r.json()
+                if "error" in response:
+                    raise commands.CommandError(response["error"])
+
+                full_response = response["stdout"] if len(response["stderr"].split('\n')) <= 4 else "\n".join(response["stderr"].split('\n')[1:-7])
+                len_response = len(full_response)
+                if len_response < 1990:
+                    msg = f"```rs\n{full_response}```"
+                elif 1990 <= len_response <= 5000:
+                    msg = await self.get_gist(full_response)
+                else:
+                    raise commands.CommandError("Output too big!")
+
+                await ctx.send(msg)
+
+    async def get_gist(self, msg):
+        data = json.dumps({"public": True, "files": {"main.rs": {"content": msg}}})
+
+        headers = {"Accept": "application/vnd.github.v3+json"}
+
+        async with self.session.post(
+                "https://api.github.com/gists", data=data, headers=headers
+        ) as r:
+            response = await r.json()
+
+        return "https://gist.github.com/anonymous/" + response["id"]
+
+    async def __error(self, ctx: commands.Context, error):
+        log.error("Playground error: %s", error)
+        if isinstance(error, (discord.HTTPException, discord.Forbidden)):
+            await ctx.send("Error while sending the output.")
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(CodeBlock.missing_error)
+        if isinstance(error, commands.CommandError):
+            await ctx.send(str(error))
 
 def setup(bot):
     bot.add_cog(Playground(bot))
+    bot.add_cog(SingleLinePlayground(bot))
