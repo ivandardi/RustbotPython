461268e82d78aa238d40fd99dec53010c9af5d78
Ivan Borgia Dardi <melo@48d238ca3594>
2018-10-11 03:24:29 +0000

    Remove pipenv and go back to pip+venv

--- deploy.sh
+++ deploy.sh
@@ -13,10 +13,12 @@ cp -a "$repo_dir" "$dest_dir"
 echo "Installed source code to '$dest_dir'"
 
 cd "$dest_dir"
-pipenv install --pre > /dev/null
+python3.6 -m venv venv > /dev/null
+source venv/bin/activate > /dev/null
+pip install -r requirements.txt > /dev/null
 echo "Installed Python dependencies"
 
-sudo chown -R ferris:ferris "$dest_dir"
+sudo chown -R melo:melo "$dest_dir"
 sudo install -m644 rustbot.service /usr/local/lib/systemd/system/rustbot.service
 sudo chown root:root /usr/local/lib/systemd/system/rustbot.service
 echo "Installed systemd service"
