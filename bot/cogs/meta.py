eb2944724be732b855d469e081f1becdeeb6d6a3
Ivan Dardi <ivanbdardi at gmail.com>
2019-01-09 13:34:06 -0200

    Add unsafe command

--- bot/cogs/meta.py
+++ bot/cogs/meta.py
@@ -8,6 +8,22 @@ class Meta:
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.rustacean_role = None
+        self.unsafe_role = None
+
+    def _setup_commands(self, ctx):
+        if not self.rustacean_role:
+            self.rustacean_role = discord.utils.get(
+                ctx.guild.roles, id=319_953_207_193_501_696
+            )
+
+        if not self.unsafe_role:
+            self.unsafe_role = discord.utils.get(
+                ctx.guild.roles, id=468_114_715_210_678_272
+            )
+
+        if not self.bot.emoji_rustok:
+            self.bot.log.info("Loading the rustOk emoji...")
+            self.bot.emoji_rustok = discord.utils.get(self.bot.emojis, name="rustOk")
 
     @commands.command()
     async def uptime(self, ctx: commands.Context):
@@ -35,19 +51,41 @@ class Meta:
         """Adds the Rustacean role to a member.
         Takes in a space-separated list of member mentions and/or IDs.
         """
-        if not self.rustacean_role:
-            self.rustacean_role = discord.utils.get(
-                ctx.guild.roles, id=319_953_207_193_501_696
-            )
-        if not self.bot.emoji_rustok:
-            self.bot.log.info("Loading the rustOk emoji...")
-            self.bot.emoji_rustok = discord.utils.get(self.bot.emojis, name="rustOk")
+
+        self._setup_commands(ctx)
+
         if self.rustacean_role not in ctx.author.roles:
             return await ctx.message.add_reaction("❌")
+
         for member in members:
             await member.add_roles(
                 self.rustacean_role, reason=f"You have been rusted by {ctx.author}! owo"
             )
+
+        await ctx.message.add_reaction(self.bot.emoji_rustok)
+
+    @commands.command()
+    @commands.guild_only()
+    async def unsafe(self, ctx: commands.Context):
+        """Adds the Unsafe role to the caller.
+        It allows access to the #unsafe channel.
+        """
+
+        self._setup_commands(ctx)
+
+        if self.rustacean_role not in ctx.author.roles:
+            return await ctx.message.add_reaction("❌")
+
+        if ctx.channel.id != 273_541_645_579_059_201:
+            await ctx.message.add_reaction("❌")
+            return await ctx.send(
+                "Why are you not running this command over on <#273541645579059201>?"
+            )
+
+        await ctx.author.add_roles(
+            self.unsafe_role, reason=f"You have been unsafed! owo"
+        )
+
         await ctx.message.add_reaction(self.bot.emoji_rustok)
 
     @commands.command()
@@ -64,15 +102,13 @@ class Meta:
 
         deleted = await ctx.channel.purge(limit=limit, check=is_me)
         await ctx.send(f"Deleted {len(deleted)} message(s)", delete_after=5)
+        await ctx.message.add_reaction(self.bot.emoji_rustok)
 
     @commands.command()
     async def source(self, ctx: commands.Context):
         """Links to the bot GitHub repo."""
 
         await ctx.send("https://github.com/ivandardi/RustbotPython")
-
-    async def __after_invoke(self, ctx: commands.Context):
-        await ctx.message.add_reaction(self.bot.emoji_rustok)
 
     async def __error(self, ctx: commands.Context, error):
         await ctx.message.clear_reactions()
