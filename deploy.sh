4913c3e4f949390f0dd44b540616633d9fabac06
Ivan Dardi <ivanbdardi at gmail.com>
2020-05-22 22:52:42 -0300

    add database backup do deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -eu
 
+echo "Fetching origin repository"
 git fetch --all
 git reset --mixed origin/master
 
-repo_dir="$(dirname "$0")"
+dt=`date '+%Y-%m-%dT%H-%M-%S'`
+echo "Making database backup ${dt}.tar.xz"
+tar -cJvf "../backup/${dt}.tar.xz" database
 
+echo "Installing Python dependencies"
 mkdir -p database
 rm -rf venv
 python3 -m venv venv
@@ -13,11 +17,14 @@ source venv/bin/activate
 pip install -r requirements.txt > /dev/null
 echo "Installed Python dependencies"
 
+echo "Installing systemd service"
+repo_dir="$(dirname "$0")"
 sudo chown -R melo:melo "$repo_dir"
 sudo install -m644 rustbot.service /usr/local/lib/systemd/system/rustbot.service
 sudo chown root:root /usr/local/lib/systemd/system/rustbot.service
 echo "Installed systemd service"
 
+echo "Starting rustbot systemd service"
 sudo systemctl daemon-reload
 sudo systemctl restart rustbot.service
 echo "Started rustbot systemd service"
